e166c4a5ee3504b5f4ec2766e3f96886cddb07bf
najafiaghdamehsan-a11y <najafiaghdam.ehsan@gmail.com>
2026-02-20 20:12:49 +0330

    UI module’s public header

---
+++ include/ui/app.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "engine/runtime.h"
+#include "model/model.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    int app_run(Project* project, Runtime* runtime);
+
+#ifdef __cplusplus
+}
+#endif
