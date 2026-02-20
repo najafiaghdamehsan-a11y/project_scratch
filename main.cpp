f60e415e4f16ba68dccf6c3b94d74d9f50488caa
najafiaghdamehsan-a11y <najafiaghdam.ehsan@gmail.com>
2026-02-20 15:31:51 +0330

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <SDL.h>
+#include <SDL_image.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <memory>        // if you decide to use smart pointers (optional)
+#include <fstream>       // save/load
+#include <sstream>
+#include <optional>
+#include <variant>       // values: number/string/bool
+#include <cmath>         // sqrt, etc
+#include <cstdint>
